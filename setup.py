6b82e9adbf44876c404425518a362c9f6433f897
myzhang1029 <myzhang1029@163.com>
2018-09-20 21:05:07 +0800

    Add skel

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/use/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = [
+    "description": "Automatic class band creator",
+    "author": "Zhang Maiyun",
+    "url": "https://github.com/myzhang1029/cbcreator",
+    "download_url": "https://github.com/myzhang1029/cbcreator",
+    "author_email": "myzhang1029@hotmail.com",
+    "version": "0.1",
+    "install_requires": ["pillow"],
+    "packages": ["cbcreator"],
+    "scripts": [],
+    "name": "cbcreator"
+]
+
+setup(**config)
