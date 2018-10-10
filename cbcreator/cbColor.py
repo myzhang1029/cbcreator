db3b638b49226051340f3226a4f2f5cd45679c9a
myzhang1029 <myzhang1029@163.com>
2018-10-10 20:22:16 +0800

    Get cbText working

--- cbcreator/cbColor.py
+++ cbcreator/cbColor.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from PIL import Image
 
 def avgcolor(imageobj):
--- cbcreator/cbText.py
+++ cbcreator/cbText.py
@@ -1,16 +1,38 @@
+from __future__ import division
 from PIL import Image, ImageDraw, ImageFont
 from random import randint
 import cbResize
 import cbColor
 
 def autowrap(text, width, draw, font):
+    """ Word-wrap text to a fixed width
+    text: the text to be wraped
+    width: the width of every line
+    draw: pillow ImageDraw object
+    font: the pillow ImageFont object to be used
+    returns the wraped text"""
+    lines = text.split('\n')
+    if ''.join(lines) != text:
+        # Already contains LFs
+        wrapped = ""
+        for line in lines:
+            wrapped += autowrap(line, width, draw, font)
+            wrapped += '\n'
+        return wrapped
     initsz = draw.textsize(text, font=font)[0]
     txtlen = len(text)
+    if txtlen == 0:
+        return ""
     # shouldn't split a character into two
-    wraploc = initsz // width - 1
+    wraploc = initsz / width # lines the text should be
+    wraploc = round(txtlen / wraploc) # lenth of every line
+    # leave some spare space for the round-off
+    wraploc -= 1
+    print(initsz, width, wraploc, txtlen)
     resultstr = ""
     for idx in range(0, txtlen):
-        if idx % wraploc == 1:
+        # insert a LF whenever the wraploc-th char is met
+        if idx % wraploc == 0 and idx != 0:
             resultstr += '\n'
         resultstr += text[idx]
     return resultstr
@@ -26,22 +48,23 @@ def create(bgfile, txtfile, title, fonttxt="", fontitle=""):
     avgcolor = cbColor.avgcolor(image)
     compcolor = cbColor.compcolor(avgcolor)
     draw = ImageDraw.Draw(image)
+    # load the font for the title and the text
     if fontitle == "":
         fontitle = "fonts/{:03}.ttf".format(randint(1, 4))
+    fontt = ImageFont.truetype(fontitle, 600, encoding="unic")
     if fonttxt == "":
         fonttxt = "fonts/{:03}.ttf".format(randint(1, 4))
-        if fonttxt == fontitle:
-            # Retry once, if it's still the same, ignore it
-            fonttxt = "fonts/{:03}.ttf".format(randint(1, 4))
-    fontt = ImageFont.truetype(fontitle, 600, encoding="unic")
-    fontx = ImageFont.truetype(fonttxt, 300, encoding="unic")
+    fontx = ImageFont.truetype(fonttxt, 100, encoding="unic")
+    # size tuple of the title
     sizet = draw.textsize(title, font=fontt)
-    text = autowrap(text, 2661 - 2 * draw.textsize(text[0], font=fontx)[0] , draw, fontx)
+    onechar = draw.textsize(title[0], font=fontx)[0]
+    # don't ask what the 3 is, it just works
+    text = autowrap(text, 2661 - 3 * onechar, draw, fontx)
     sizex = draw.textsize(text, font=fontx)
     leftt = (2661 - sizet[0]) // 2
     # Set margin to the width of a character
-    leftx = draw.textsize(text[0], font=fontx)[0]
-    top = (3072 - 2 * sizet[1] - sizex[1]) // 2
+    leftx = (2661 - sizex[0]) // 2
+    top = (3072 - sizet[1] - sizex[1] - onechar) // 2
     draw.text((leftt, top), title, font=fontt, fill=compcolor)
-    draw.text((leftx, top + 2 * sizet[1]), text, font=fontx, fill=compcolor)
+    draw.text((leftx, top + sizet[1] + onechar), text, font=fontx, fill=compcolor)
     return image
