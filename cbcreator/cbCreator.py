a873c1311ef59dac30178c6caabb8003cb547148
myzhang1029 <myzhang1029@163.com>
2018-10-20 10:45:11 +0800

    Add license notice to the source files

--- cbcreator/cbCreator.py
+++ cbcreator/cbCreator.py
@@ -1,3 +1,19 @@
+# cbcreator.py - main routines to render a class band slide
+#
+# Copyright 2018 Zhang Maiyun
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import print_function, division
 from sys import argv, exit, stdin, stdout, stderr, version_info
 from os.path import realpath, dirname, isfile
